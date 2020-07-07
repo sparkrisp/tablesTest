4c7a1948101ec3b7d98e1b8c170f63531025808a
Ariel Ivan Ruiz Mateos <arielrm@gmail.com>
2020-07-07 04:16:34 +0200

    Add files via upload

---
+++ cordova.js
@@ -0,0 +1,6 @@
+/*!
+This file is part of DecSoft App Builder.
+Visit our website for license information.
+Copyright ©2020 DecSoft. All rights reserved.
+Visit our website at: https://www.davidesperalta.com
+*/
